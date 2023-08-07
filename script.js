fe1afcf5e3f1a247cafe692b46f67e32c48098dd
Jevaughn Vickers <fullstackjev@gmail.com>
2023-08-07 07:58:13 -0400

    remove redundacies in checkForWin function

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ let playOne = {sign: 'X', playLocation: '4'}
 
 let playTwo = {sign: 'O', playLocation: '9'}
 
-let playThree = {sign: 'X', playLocation: '5'}
+let playThree = {sign: 'X', playLocation: '1'}
 
 let playFour = {sign: 'O', playLocation: '2'}
 
-let playFive = {sign: 'X', playLocation: '6'}
+let playFive = {sign: 'X', playLocation: '7'}
 
 let playSix = {sign: 'O', playLocation: '3'}
 
@@ -103,37 +103,11 @@ const Game = (() => {
                         .every(nextBlock => nextBlock.textContent === block.textContent && nextBlock.textContent !== ""),
                         blockGroup: Array.from(Gameboard.blocks)
                         .filter(block => block.id > "7" && block.id <= "9" || block.id === "7")
-                    },
-                    // up: {
-                    //     win: Array.from(Gameboard.blocks).filter(block => block.id === "1" || block.id === "4")
-                    //     .every(nextBlock => nextBlock.textContent === block.textContent && nextBlock.textContent !== ""),
-                    //     blockGroup: Array.from(Gameboard.blocks)
-                    //     .filter(block => block.id === "1" || block.id === "4" || block.id === "7")
-                    // }
+                    }
                 };
                 for(let dir in check){
                     if(check[dir].win) console.log(check[dir].blockGroup, block.textContent);
                 };
-     
-            }else if(block.id === "9"){
-                // bottom right corner block
-                // const check = {
-                //     across: {
-                //         win: Array.from(Gameboard.blocks).filter(block => block.id > "6" && block.id < "9")
-                //         .every(nextBlock => nextBlock.textContent === block.textContent && nextBlock.textContent !== ""),
-                //         blockGroup: Array.from(Gameboard.blocks)
-                //         .filter(block => block.id > "6" && block.id < "9" || block.id === "9")
-                //     },
-                //     // up: {
-                //     //     win: Array.from(Gameboard.blocks).filter(block => block.id === "6" || block.id === "3")
-                //     //     .every(nextBlock => nextBlock.textContent === block.textContent && nextBlock.textContent !== ""),
-                //     //     blockGroup: Array.from(Gameboard.blocks)
-                //     //     .filter(block => block.id === "6" || block.id === "3" || block.id === "9")
-                //     // }
-                // };
-                // for(let dir in check){
-                //     if(check[dir].win) console.log(check[dir].blockGroup, block.textContent);
-                // };
 
             }else if(block.id === "5"){
                 // middle block
